f50ccb5d4d5c22ccc7c472f5a6ad3874c559b12c
Georgio Feghali <giorgiofeghali2002@icloud.com>
2022-07-19 18:20:31 -0700

    Modified colors and sizes.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
     <div name='home' className='w-full h-screen bg-[#0a192f]'>
       
       {/* Comtainer */}
-      <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
+      <div className='max-w-[1020px] mx-auto px-8 flex flex-col justify-center h-full'>
         <p className='text-pink-600'>Hi there, my name is</p>
         <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6] pb-2'>Georgio Feghali</h1>
         <div className='flex items-center text-3xl sm:text-7xl font-bold text-[#8892b0]'>
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -37,12 +37,12 @@ const NavBar = () => {
 
       {/* Social icons */}
       <div className='hidden lg:flex fixed flex-col top-[35%] left-0'>
-        <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] bg-blue-600 hover:ml-[-10px] duration-300'>
+        <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] bg-[#0A66C2] hover:ml-[-10px] duration-300'>
           <a className='flex justify-between items-center w-full text-gray-300' href='https://www.linkedin.com/in/georgio-feghali/'>
             LinkedIn <FaLinkedin size={30}/>
           </a>
         </li>
-        <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] bg-gray-700 hover:ml-[-10px] duration-300'>
+        <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] bg-[#181717] hover:ml-[-10px] duration-300'>
           <a className='flex justify-between items-center w-full text-gray-300' href='https://github.com/GeorgioFe'>
             Github <FaGithub size={30}/>
           </a>
